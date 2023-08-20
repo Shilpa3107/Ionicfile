4fa31a51585e655e35952c106eeddaaca75cfb2b
Shilpa <shilpa.sinha3107@gmail.com>
2023-08-20 22:25:43 +0530

    Changing sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -6,7 +7,8 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  dataurl: string='./assets/home.json';
+  // dataurl: string= './assets/home.json';
+  dataurl:string ="";
   items: any;
   public expandedSubtopics: { [key: string]: boolean } = {};
 
@@ -22,28 +24,42 @@ export class AppComponent {
     return this.expandedSubtopics[label.name] === true;
   }
   public appPages = [
-    { title: 'Home', url: '/folder/home', icon: 'home',content:'Welcome to home component' },
-    { title: 'Methodology', url: '/folder/methodology', icon: 'bulb' },
-    { title: 'Case Studies', url: '/folder/casestudies', icon: 'book' },
-    { title: 'Best Practices', url: '/folder/practices', icon:'laptop' },
-    { title: 'Tool & Resources', url: '/folder/tool', icon: 'hammer' },
+    { title: 'Home', url: '/folder/home', icon: 'home',lastword: 'home' },
+    { title: 'Methodology', url: '/folder/methodology', icon: 'bulb'},
+    { title: 'Case Studies', url: '/folder/casestudies', icon: 'book',lastword:'casestudies' },
+    { title: 'Best Practices', url: '/folder/practices', icon:'laptop' ,lastword:'practices'},
+    { title: 'Tool & Resources', url: '/folder/tool', icon: 'hammer',lastword:'tool' },
     { title: 'Blog', url: '/folder/blog', icon: 'pencil' },
     { title: 'About Us', url: '/folder/about', icon: 'people'},
     { title: 'Contact Us', url: '/folder/contact', icon:'mail'}
   ];
+
+  
   public selectedTopic: string ='home';
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {
-    fetch(this.dataurl).then(res => res.json())
-    .then(json => {
-      this.items = json.topics;
-      
-      console.log("Topic name : ",this.items);
-     
-    }); 
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const segments = event.url.split('/').filter(segment => segment !== '');
+        const lastSegment = segments[segments.length - 1];
+        const menuItem = this.appPages.find(page => page.lastword === lastSegment);
+        if (menuItem) {
+          // this.selectedTopic = menuItem.lastword;
+          this.dataurl = `/assets/${menuItem.lastword}.json`; // Correctly use backticks for template literals
+          this.fetchData();
+        }
+      }
+    });
   }
 
-  public selectTopic(topic: string):void {
-    this.selectedTopic = topic;
+  private fetchData(): void {
+    fetch(this.dataurl)
+      .then(res => res.json())
+      .then(json => {
+        this.items = json.topics;
+        console.log('Topic name : ', this.items);
+      });
   }
+  
+
 }
