650d51bb2818bccbecca58ede77e7a1c1f78371c
Shilpa <shilpa.sinha3107@gmail.com>
2023-08-20 21:05:27 +0530

    Showing the sidebar topic's name and its subtopic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,23 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+  dataurl: string='./assets/home.json';
+  items: any;
+  public expandedSubtopics: { [key: string]: boolean } = {};
+
+  public toggleSubtopic(label: any): void {
+    if (this.expandedSubtopics[label.name]) {
+      this.expandedSubtopics[label.name] = false;
+    } else {
+      this.expandedSubtopics[label.name] = true;
+    }
+  }
+
+  public isSubtopicExpanded(label: any): boolean {
+    return this.expandedSubtopics[label.name] === true;
+  }
   public appPages = [
-    { title: 'Home', url: '/folder/home', icon: 'home' },
+    { title: 'Home', url: '/folder/home', icon: 'home',content:'Welcome to home component' },
     { title: 'Methodology', url: '/folder/methodology', icon: 'bulb' },
     { title: 'Case Studies', url: '/folder/casestudies', icon: 'book' },
     { title: 'Best Practices', url: '/folder/practices', icon:'laptop' },
@@ -16,6 +31,19 @@ export class AppComponent {
     { title: 'About Us', url: '/folder/about', icon: 'people'},
     { title: 'Contact Us', url: '/folder/contact', icon:'mail'}
   ];
+  public selectedTopic: string ='home';
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  constructor() {
+    fetch(this.dataurl).then(res => res.json())
+    .then(json => {
+      this.items = json.topics;
+      
+      console.log("Topic name : ",this.items);
+     
+    }); 
+  }
+
+  public selectTopic(topic: string):void {
+    this.selectedTopic = topic;
+  }
 }
