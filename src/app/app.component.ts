3ab004bfe666bf3372ca94647b6ffa9c2253e9c8
Shilpa <shilpa.sinha3107@gmail.com>
2023-08-28 19:38:12 +0530

    Removed the changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,10 +49,7 @@ export class AppComponent {
           // Correctly use backticks for template literals
           this.fetchData();
         }
-        else {
-          this.selectedMenu = ''; // Clear selectedMenu if no matching lastword is found
-          this.items = []; // Clear items to show an empty list in the sidebar
-        }
+        
       }
     });
   }
