68c2b5b1a132fa2f63a9a2bcc43ab393c959a083
Shilpa <shilpa.sinha3107@gmail.com>
2023-09-05 07:02:30 +0530

    imported the search page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'folder/:id/:topic',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
   }
 ];
 
