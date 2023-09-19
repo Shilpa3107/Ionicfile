0a2e8f953d881a65878bb5b9f27bd439888fdc2c
Shilpa <shilpa.sinha3107@gmail.com>
2023-09-19 16:34:40 +0530

    Created login page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FireserviceService } from 'src/app/fireservice.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+email: any;
+password: any;
 
-  constructor() { }
+
+  constructor(public router: Router,
+    public fireservice: FireserviceService) { }
 
   ngOnInit() {
   }
 
+  navigatetoLogin() {
+    this.fireservice.loginWithEmail({ email: this.email, password: this.password }).then((res: any) => {
+      console.log(res);
+      if (res.user.uid) {
+        this.fireservice.getDetails({ uid: res.user.uid }).subscribe({
+          next: (res: any) => {
+            console.log(res);
+            alert('Welcome ' + res['name']);
+          },
+          error: (err: any) => {
+            console.log(err);
+          }
+        });
+      }
+    }, err => {
+      alert(err.message)
+      console.log(err);
+    });
+    // this.router.navigate(['/login']);
+  }
+  
+  navigatetosignup(){
+    this.router.navigate(['/signup']);
+  }
+
+  
 }
