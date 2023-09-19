17273b29e6ca5e2c39e8fdd7d26097303448ef07
Shilpa <shilpa.sinha3107@gmail.com>
2023-09-19 17:00:45 +0530

    Created signup page

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FireserviceService } from 'src/app/fireservice.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
-
-  constructor() { }
+  public name: any;
+  public email: any;
+  public password: any;
+  constructor(
+    public router: Router,
+    public fireservice: FireserviceService
+  ) { }
 
   ngOnInit() {
+  }
+  signup(){
+   this.fireservice.signup({email:this.email, password: this.password}).then((res: any)=>{
+    if(res.user.uid){
+      let data={
+        email:this.email,
+        password:this.password,
+        name:this.name,
+        uid:res.user.uid
+      }
+      this.fireservice.saveDetails(data).then((res:any)=>{
+        alert('Account Created!');
+      },err=>{
+        console.log(err);
+      })
+    }
+  },err=>{
+    alert(err.message);
+
+    console.log(err);
+   }
+   )
   }
 
 }
